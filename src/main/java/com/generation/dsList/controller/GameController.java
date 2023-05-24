5ddd9ec92b28ca34963918f5844c1096e891cbae
kaiquesilvadev <j.kaique1570@gmail.com>
2023-05-24 17:19:55 -0300

    get game by id

--- src/main/java/com/generation/dsList/controller/GameController.java
+++ src/main/java/com/generation/dsList/controller/GameController.java
@@ -5,9 +5,11 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.generation.dsList.dto.GameDTO;
 import com.generation.dsList.dto.GameMinDTO;
 import com.generation.dsList.services.GameServices;
 
@@ -22,6 +24,13 @@ public class GameController {
 	@GetMapping
 	public List<GameMinDTO> findAll() {
 		List<GameMinDTO> result = gameServices.findall();
+		return result;
+	}
+	
+	// buscar por id (o método esta na classe service aqui ele só  faz a requisição )
+	@GetMapping("/{id}")
+	public GameDTO findById(@PathVariable  Long id) {
+		GameDTO result = gameServices.findByid(id);
 		return result;
 	}
 }
--- src/main/java/com/generation/dsList/services/GameServices.java
+++ src/main/java/com/generation/dsList/services/GameServices.java
@@ -4,22 +4,35 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.generation.dsList.dto.GameDTO;
 import com.generation.dsList.dto.GameMinDTO;
 import com.generation.dsList.entities.Game;
 import com.generation.dsList.repository.GameRepository;
 
- // essa anotação serve para definir que essa classe vai ser usada para servicos 
- // você pode usar também a anotação @Component que indentifivca que essa classe serrar usada como classe de composição
+// essa anotação serve para definir que essa classe vai ser usada para servicos 
+// você pode usar também a anotação @Component que indentifivca que essa classe serrar usada como classe de composição
 
 @Service
 public class GameServices {
 
 	@Autowired
 	private GameRepository gameRepository;
-	
+
+	// busca por id
+
+	               // para indicar que um método ou classe deve ser executado em uma
+	@Transactional(readOnly = true) // transação.(inpont spring , não é do jakarta)
+	public GameDTO findByid(Long id) {
+		Game result = gameRepository.findById(id).get();
+		return new GameDTO(result);
+
+	}
+	// reporna o GameMinDTO com os atributos personalisado para a requisição
+	@Transactional(readOnly = true)
 	public List<GameMinDTO> findall() {
 		List<Game> result = gameRepository.findAll();
-		return result.stream().map(x -> new  GameMinDTO(x)).toList();
+		return result.stream().map(x -> new GameMinDTO(x)).toList();
 	}
 }
